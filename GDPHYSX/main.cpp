39bff138afc5e6b74dae0e06f87beaeb0bc3e7b2
jillebora <jillana_ebora@dlsu.edu.ph>
2026-06-04 18:53:59 +0800

    Update main.cpp

--- GDPHYSX/main.cpp
+++ GDPHYSX/main.cpp
@@ -10,13 +10,18 @@
 #include <vector>
 #include <list>
 #include <chrono>
+#include <algorithm>
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 
 
-#include "P6/particle.h"
+//#include "P6/particle.h"
+#include "physicsWorld.h"
 #include "renderParticle.h"
+#include "shader.h"
+#include "model.h"
+#include "dragForceGenerator.h"
 
 using namespace std;
 using namespace std::chrono_literals;
@@ -30,187 +35,101 @@ int main()
 		return -1;
 	}
 
-	float windowWidth = 800;
-	float windowHeight = 800;
+	float windowWidth = 700;
+	float windowHeight = 700;
 
-	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Jillana Ebora", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "PC01 Jillana Ebora", NULL, NULL);
+
+	P6::PhysicsWorld pWorld = P6::PhysicsWorld();
+
+
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
-	// SHADERS
-
-	std::fstream vertSrc("Shaders/sphere.vert");
-	std::stringstream vertBuff;
-	//add the file stream to the string stream
-	vertBuff << vertSrc.rdbuf();
-
-	std::string vertS = vertBuff.str();
-	const char* v = vertS.c_str();
-
-	std::fstream fragSrc("Shaders/sphere.frag");
-	std::stringstream fragBuff;
-
-	fragBuff << fragSrc.rdbuf();
-
-	std::string fragS = fragBuff.str();
-	const char* f = fragS.c_str();
-
 	glfwMakeContextCurrent(window);
-
 	gladLoadGL();
-
 	glEnable(GL_DEPTH_TEST);
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// SHADERS
+	Shader shader("Shaders/sphere.vert", "Shaders/sphere.frag");
 
-	glShaderSource(vertexShader, 1, &v, NULL);
-
-	glCompileShader(vertexShader);
-
-	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	glShaderSource(fragShader, 1, &f, NULL);
-
-	glCompileShader(fragShader);
-
-	GLuint shaderProg = glCreateProgram();
-	glAttachShader(shaderProg, vertexShader);
-	glAttachShader(shaderProg, fragShader);
-
-	glLinkProgram(shaderProg);
-
-	GLfloat vertices[]{
-		0.f, 0.5f, 0.f,	  //0
-		-1.f, -0.5f, 0.f, //1
-		0.5f, -0.5f, 0.f  //2
-	};
-
-	GLuint indices[]{
-		0,1,2
-	};
-
-	// LOAD OBJ
-
-	string path = "3D/sphere.obj";
-	vector<tinyobj::shape_t> shapes;
-	vector<tinyobj::material_t> material;
-	string error;
-	string warning;
-
-
-	tinyobj::attrib_t attributes;
-
-	bool success_obj = tinyobj::LoadObj(
-		&attributes,
-		&shapes,
-		&material,
-		&warning,
-		&error,
-		path.c_str()
-	);
-
-
-	vector<GLuint> mesh_indices;
-	for (int i = 0; i < shapes[0].mesh.indices.size(); i++)
-	{
-		mesh_indices.push_back(shapes[0].mesh.indices[i].vertex_index);
-	}
-
-	// OPENGL BUFFERS
-
-	GLuint VAO, VBO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-
-	glBufferData(GL_ARRAY_BUFFER,
-		sizeof(GLfloat) * attributes.vertices.size(),
-		&attributes.vertices[0],
-		GL_STATIC_DRAW);
-
-
-	glVertexAttribPointer(0,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
-		3 * sizeof(GLfloat),
-		(void*)0);
-
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-
-
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * mesh_indices.size(), mesh_indices.data(), GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	// MODEL
+	Model sphere;
+	sphere.loadFromObj("3D/sphere.obj");
+	sphere.setScale(glm::vec3(10.f));
 
 	// CAMERA
 
-	glm::mat4 proj = glm::ortho(
-		-400.f, 400.f,
-		-400.f, 400.f,
-		-1.f, 1000.f
-	);
-
-	glm::vec3 cameraPos =
-		glm::vec3(0.f, 0.f, 5.f);
+	glm::mat4 proj = glm::ortho(-350.f, 350.f, -350.f, 350.f, -500.f, 1000.f);
 
 	glm::mat4 view = glm::lookAt(
-		cameraPos,
+		glm::vec3(0.f, 0.f, 700.f),
 		glm::vec3(0.f, 0.f, 0.f),
-		glm::vec3(0.f, 1.f, 0.f)
-	);
+		glm::vec3(0.f, 1.f, 0.f));
 
-	// PARTICLE
+	// Wire shader + camera into the model's RenderObject
+	sphere.setShader(&shader.ID);
+	sphere.setCamera(&proj, &view);
 
-	P6::Particle particle;
+	// ==============================
+	// ========= PARTICLES ==========
+	// ==============================
 
-	particle.Position = glm::vec3(0.f, 0.f, 0.f);
-	particle.Velocity = glm::vec3(100.f, 0.f, 0.f);
-	particle.Acceleration = glm::vec3(-30.f, 0.f, 0.f);
+	// RED PARTICLE
+	P6::Particle p1;
+	p1.setName("Red");
+	p1.Position = glm::vec3(-400, 200, 0);
+	p1.Velocity = P6::Particle::makeVec(80.f, p1.Position);
+	p1.Acceleration = P6::Particle::makeVec(14.5f, p1.Position);
+	pWorld.AddParticle(&p1);
 
-	// RENDER OBJECT
+	// YELLOW PARTICLE
 
-	RenderObject sphereObj;
+	P6::Particle p2;
+	p2.setName("Yellow");
+	p2.Position = glm::vec3(-400.f, 0.f, 0);
+	p2.Velocity = P6::Particle::makeVec(110.f, p2.Position);
+	p2.Acceleration = P6::Particle::makeVec(3.f, p2.Position);
+	pWorld.AddParticle(&p2);
 
-	sphereObj.shaderProg = &shaderProg;
+	// GREEN PARTICLE
 
-	sphereObj.VAO = &VAO;
+	P6::Particle p3;
+	p3.setName("Green");
+	p3.Position = glm::vec3(-400.f, -200.f, 0);
+	p3.Velocity = P6::Particle::makeVec(90.f, p3.Position);
+	p3.Acceleration = P6::Particle::makeVec(8.f, p3.Position);
+	pWorld.AddParticle(&p3);
 
-	sphereObj.projection = &proj;
+	// DRAG
+	DragForceGenerator drag = DragForceGenerator(0.14, 0.1);
+	pWorld.forceRegistry.Add(&p1, &drag);
 
-	sphereObj.view = &view;
+	// RENDER PARTICLE 
+	RenderParticle rp1(&p1, sphere.getRenderObject(), glm::vec3(1.f, 0.f, 0.f));
+	RenderParticle rp2(&p2, sphere.getRenderObject(), glm::vec3(1.f, 1.f, 0.f));
+	RenderParticle rp3(&p3, sphere.getRenderObject(), glm::vec3(0.f, 1.f, 0.f));
 
-	sphereObj.indexCount =
-		mesh_indices.size();
+	rp1.Scale = glm::vec3(5.f);
+	rp2.Scale = glm::vec3(5.f);
+	rp3.Scale = glm::vec3(5.f);
 
+	// MOVEMENT VECTOR
 
-	// RENDER PARTICLE
-
-	RenderParticle rp(
-		&particle,
-		&sphereObj,
-		glm::vec3(1.f, 0.f, 0.f)
-	);
-
-	rp.Scale = glm::vec3(100.f);
+	vector<P6::Particle*> particles;
+	particles.push_back(&p1);
+	particles.push_back(&p2);
+	particles.push_back(&p3);
 
 	// LIST
 
 	list<RenderParticle*> RenderParticles;
-
-	RenderParticles.push_back(&rp);
+	RenderParticles.push_back(&rp1);
+	RenderParticles.push_back(&rp2);
+	RenderParticles.push_back(&rp3);
 
 	// TIME
 
@@ -222,6 +141,9 @@ int main()
 	auto prev_time = curr_time;
 
 	std::chrono::nanoseconds curr_ns(0);
+
+	int currentRank = 1;
+	bool printedResults = false;
 
 	// LOOP
 
@@ -241,13 +163,29 @@ int main()
 
 		if (curr_ns >= timestep)
 		{
-			constexpr float timestep_sec =
-				timestep.count() / (float)1E09;
+			constexpr float timestep_sec = timestep.count() / (float)1E09;
 
 			curr_ns -= timestep;
 
-			particle.Update(timestep_sec);
+			pWorld.Update(timestep_sec);
+
+			for (auto* p : pWorld.GetParticles())
+			{
+				// when Particle reaches center
+				/*if (glm::length(p->Position) <= 1.f)
+				{
+					if (p == &p1)
+					{
+						p1.Destroy();
+					}
+				}*/
+				if (p == &p1 && p->Position.x >= 0.f)
+				{
+					p1.Destroy();
+				}
+			}
 		}
+
 
 		glfwPollEvents();
 
@@ -255,25 +193,14 @@ int main()
 
 		// DRAW LIST
 
-		for (
-			list<RenderParticle*>::iterator i =
-			RenderParticles.begin();
-
-			i != RenderParticles.end();
-
-			i++
-			)
-		{
-			(*i)->Draw();
-		}
+		for (auto* rp : RenderParticles)
+			rp->Draw();
 
 		glfwSwapBuffers(window);
 	}
 
-	//clean
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	sphere.destroy();
+	shader.destroy();
 
 	glfwTerminate();
 	return 0;

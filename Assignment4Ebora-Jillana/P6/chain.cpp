fe2869be4d772b230b13e6a025e499ae8c2d7a38
jillebora <jillana_ebora@dlsu.edu.ph>
2026-06-22 06:55:57 +0800

    Assignment4 first push

--- Assignment4Ebora-Jillana/P6/chain.cpp
+++ Assignment4Ebora-Jillana/P6/chain.cpp
@@ -1,16 +1,15 @@
-#include "rod.h"
-#include "rod.h"
+#include "chain.h"
 
-ParticleContact* Rod::GetContact()
+ParticleContact* Chain::GetContact()
 {
 	float currLen = CurrentLength();
 
-	if (currLen == length)
+	if (currLen < +maxLength)
 		return nullptr;
 
 	if (currLen == 0.f)
 		return nullptr;
-	
+
 	ParticleContact* ret = new ParticleContact();
 	ret->particles[0] = particles[0];
 	ret->particles[1] = particles[1];
@@ -18,19 +17,8 @@ ParticleContact* Rod::GetContact()
 	glm::vec3 dir = particles[1]->Position - particles[0]->Position;
 	dir = glm::normalize(dir);
 
-	if (currLen > length)
-	{
-		ret->contactNormal = dir;
-		ret->depth = currLen - length;
-	}
-	else
-	{
-		ret->contactNormal = dir * -1.f;
-		ret->depth = length - currLen;
-	}
-
-	ret->restitiution = restitution;
+	ret->contactNormal = dir;
+	ret->depth = currLen - maxLength;
 
 	return ret;
-
 }
---
+++ Assignment4Ebora-Jillana/P6/rod.cpp
@@ -0,0 +1,40 @@
+#include "rod.h"
+#include <iostream>
+
+ParticleContact* Rod::GetContact()
+{
+	float currLen = CurrentLength();
+
+	if (currLen == length)
+		return nullptr;
+
+	if (currLen == 0.f)
+		return nullptr;
+	
+	ParticleContact* ret = new ParticleContact();
+	ret->particles[0] = particles[0];
+	ret->particles[1] = particles[1];
+
+	glm::vec3 dir = particles[1]->Position - particles[0]->Position;
+	dir = glm::normalize(dir);
+
+	if (currLen > length)
+	{
+		ret->contactNormal = dir;
+		ret->depth = currLen - length;
+	}
+	else
+	{
+		ret->contactNormal = dir * -1.f;
+		ret->depth = length - currLen;
+	}
+
+	ret->restitiution = restitution;
+
+	std::cout << "Current Length: " << currLen
+		<< "  Rod Length: " << length
+		<< "  Depth: " << ret->depth << std::endl;
+
+	return ret;
+
+}
